6bbe6394a19a2ae11cde414b9c09a63b6a4b7deb
Bram Stolk <b.stolk@gmail.com>
2017-11-20 21:40:48 -0800

    Clean up.

--- Makefile
+++ Makefile
@@ -1,13 +1,26 @@
-
+CC=clang-3.8
 
 testsino: sino.c testsino.c
-	clang-3.5 -O3 -Wall -Wno-missing-braces -ffast-math -std=gnu99 sino.c testsino.c -o testsino
+	$(CC) -O3 -Wall -Wno-missing-braces -ffast-math -std=gnu99 sino.c testsino.c -o testsino -lm
 
 
 # Test run produces 512 frames of 512x512 pixels that are assembled into an animated gif.
 # Each frame is a z-slice through a 3D noise volume.
 run: testsino
-	time ./testsino 200
-	convert -delay 3 -loop 0 out????.pgm images/anim.gif
+	time ./testsino 256
+	convert -delay 3 -loop -1 out????.pgm images/anim.gif
 
+
+testsimd: testsimd.c sino_3d.ispc sino.c
+	ispc -O2 --arch=x86-64 --target=avx2 sino_3d.ispc -o sino_3d_simd.o -h sino_3d_simd.h
+	$(CC) -o testsimd testsimd.c sino_3d_simd.o sino.c
+
+runsimd: testsimd
+	time ./testsimd 256
+
+clean:
+	rm -f *.o
+	rm -f sino_3d_simd.h
+	rm -f testsino
+	rm -f testsimd
 
--- testsino.c
+++ testsino.c
@@ -4,47 +4,25 @@
 #include <assert.h>
 
 #include "sino.h"
-
-void write_pgm( FILE*f, scalar* im, int sz )
-{
-	unsigned char* data = ( unsigned char* ) malloc( sz * sz );
-	scalar* reader = im;
-	unsigned char* writer = data;
-	for ( int y=0; y<sz; ++y )
-		for ( int x=0; x<sz; ++x )
-		{
-			const int v = 255.99999f * (*reader++);
-			*writer++ = (unsigned char) v;
-		}
-	fprintf( f, "P5\n%d %d\n255\n", sz, sz );
-	fwrite( data, sz*sz, 1, f );
-}
+#include "write_pgm.h"
 
 
 static void do_frame( int nr, scalar*im, int sz )
 {
 	scalar* writer = im;
-	// Set the frequencies and amplitudes for the four octaves.
+
+	// Set the frequency
 	const float f0 = 1.0f / sz * 3;
-	const float f1 = 2.0f / sz * 3;
-	const float f2 = 4.0f / sz * 3;
-	const float f3 = 8.0f / sz * 3;
-	const float a0 = 1.0f;
-	const float a1 = 0.5f;
-	const float a2 = 0.25f;
-	const float a3 = 0.125f;
+
 	// Pick a z-slice based on the framenr.
-	const float z = nr * 2.0f / sz;
+	const float z = nr * 0.90f / sz;
+
 	// Generate a 2d image.
 	for ( int y=0; y<sz; ++y )
 		for ( int x=0; x<sz; ++x )
 		{
-			const scalar v0 = a0 * sino_3d( x * f0 , y * f0, z );
-			const scalar v1 = a1 * sino_3d( x * f1 , y * f1, z );
-			const scalar v2 = a2 * sino_3d( x * f2 , y * f2, z );
-			const scalar v3 = a3 * sino_3d( x * f3 , y * f3, z );
-			const scalar v = ( v0 + v1 + v2 + v3 ); 		// -1.875 .. 1.875
-			*writer++ = ( v + 1.875f ) / ( 2 * 1.875 );
+			scalar v = sino_3d_4o( x * f0, y * f0, z );	// -1 .. 1
+			*writer++ = ( v + 1.0f ) / ( 2.0f );		//  0 .. 1
 		}
 }
 
@@ -53,7 +31,7 @@ int main( int argc, char* argv[] )
 {
 	if ( argc != 2 )
 	{
-		fprintf( stderr, "Usage: %s imsize", argv[ 0 ] );
+		fprintf( stderr, "Usage: %s imsize\n", argv[ 0 ] );
 		exit( 1 );
 	}
 	int sz = atoi( argv[ 1 ] );
